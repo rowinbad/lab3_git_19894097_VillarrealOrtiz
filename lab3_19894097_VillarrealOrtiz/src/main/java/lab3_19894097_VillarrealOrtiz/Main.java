2617ae0c4c7a426290287bdd7165fa9388eff360
Pablo Villarreal <pablo.villarreal@usach.cl>
2021-08-08 13:28:51 -0400

    creacion de menu interactivo

--- lab3_19894097_VillarrealOrtiz/src/main/java/lab3_19894097_VillarrealOrtiz/Main.java
+++ lab3_19894097_VillarrealOrtiz/src/main/java/lab3_19894097_VillarrealOrtiz/Main.java
@@ -5,17 +5,44 @@
  */
 package lab3_19894097_VillarrealOrtiz;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rowin
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int activo = 0;
+        Scanner entrada = new Scanner(System.in);
+        int menu = 1;
+        while (menu != 0){
+            System.out.println("### RED SOCIAL : GramInsta ###");
+            if (activo == 0){
+                System.out.println("### Authentication ###");
+                System.out.println("Elija su opcion");
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println("0. Salir del programa");
+                menu = entrada.nextInt();
+                //varios if para las funciones
+            }
+            else {
+                System.out.println("### Registrado como: (getUsuario a partir de ID ###");
+                System.out.println("Elija su opcion");
+                System.out.println("1. Realizar publicacion");  //funcionalidad Post
+                System.out.println("2. Seguir a un usuario");   //funcionalidad Follow
+                System.out.println("3. Compartir publicacion"); //funcionalidad Share
+                System.out.println("4. Visualizar red social"); //funcionalidad visualize
+                System.out.println("5. Cerrar sesión");         //funcionalidad Logout
+                System.out.println("0. Salir del programa");
+                menu = entrada.nextInt();
+                //varios if para las funciones
+            }
+        }
+        System.out.println("Cerrando Programa");
     }
     
 }
